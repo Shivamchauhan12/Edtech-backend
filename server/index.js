c9fa56fd71f0b82f3267c35ee1fc5f9cbdf37c85
Shivamchauhan12 <shivamchauhan57332@gmail.com>
2024-12-05 00:27:29 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
+	  origin: "*", // Allow all origins
+	  credentials: true, // Optional: Include credentials like cookies or authorization headers
 	})
-)
+  );
 
 app.use(
 	fileUpload({
